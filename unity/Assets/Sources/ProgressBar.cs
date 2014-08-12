648df291db7e918f7766191514f294a0df8cee68
Jan Rabe <jan.rabe@wooga.net>
2014-08-12 22:52:17 +0200

    progress bar

---
+++ unity/Assets/Sources/ProgressBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Sources
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        public Transform Bar;
+        private float _startPosX;
+
+        [Range(0,1)]
+        public float Percent;
+
+        public void Update()
+        {
+            SetProgress(Percent);
+        }
+
+        public void Awake()
+        {
+            _startPosX = Bar.transform.localScale.x;
+        }
+
+        public void SetProgress(float percent)
+        {
+            var pos = Bar.transform.localScale;
+            pos.x = _startPosX*percent;
+            Bar.transform.localScale = pos;
+        }
+    }
+}

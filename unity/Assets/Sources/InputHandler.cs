dc70f561c9d93166ea63382aabec247e4b4b10ee
Jan Rabe <jan.rabe@wooga.net>
2014-08-13 22:20:11 +0200

    2 images

---
+++ unity/Assets/Sources/InputHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Sources
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public UITexture Original;
+        public UITexture Fake;
+        public Camera camera;
+        public Vector3 Position;
+
+        public void Start()
+        {
+            UIEventListener.Get(Original.gameObject).onClick += (go) =>
+            {
+                RaycastHit hit;
+                if (!Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+                    return;
+                
+                var tex = Original.mainTexture as Texture2D;
+
+                Vector3 pixelUV = Original.transform.InverseTransformPoint(hit.point);
+                pixelUV.x *= tex.width;
+                pixelUV.y *= tex.height;
+
+                Debug.Log(hit.textureCoord);
+            };
+
+        }
+    }
+}
---
+++ unity/Assets/Sources/TextureHit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Sources
+{
+    public class TextureHit : MonoBehaviour
+    {
+        public Camera Camera;
+
+        public void Update()
+        {
+            if (!Input.GetMouseButton(0))
+                return;
+            
+            RaycastHit hit;
+            if (!Physics.Raycast(Camera.ScreenPointToRay(Input.mousePosition), out hit))
+                return;
+            Debug.Log("test3");
+
+            var renderer = hit.collider.renderer;
+            var meshCollider = hit.collider as MeshCollider;
+            if (renderer == null || renderer.sharedMaterial == null || renderer.sharedMaterial.mainTexture == null || meshCollider == null)
+                return;
+
+            Debug.Log("hit " + hit.textureCoord);
+
+            var tex = (Texture2D) renderer.material.mainTexture;
+            var pixelUV = hit.textureCoord;
+            pixelUV.x *= tex.width;
+            pixelUV.y *= tex.height;
+            tex.SetPixel((int) pixelUV.x, (int) pixelUV.y, Color.black);
+            tex.Apply();
+        }
+    }
+}

0cf779f1ab25858591fdc521076cec860d980499
Jan Rabe <jan.rabe@wooga.net>
2014-08-14 11:39:47 +0200

    fancy animation on hit

--- unity/Assets/Sources/InputHandler.cs
+++ unity/Assets/Sources/InputHandler.cs
@@ -7,21 +7,25 @@ namespace Assets.Sources
         public UITexture Original;
         public UITexture Fake;
         public Camera camera;
+        public GameObject ShockWave;
         public Vector3 Position;
 
         public void Start()
         {
-            UIEventListener.Get(Original.gameObject).onClick += (go) => Debug.Log(GetUVHit(Original, camera));
-            UIEventListener.Get(Fake.gameObject).onClick += (go) => Debug.Log(GetUVHit(Fake, camera));
+            UIEventListener.Get(Original.gameObject).onClick += (go) => Debug.Log( GetUVHit(Original));
+            UIEventListener.Get(Fake.gameObject).onClick += (go) => Debug.Log(GetUVHit(Fake));
         }
 
-        public static Vector3 GetUVHit(UITexture go, Camera camera)
+        public Vector3 GetUVHit(UITexture go)
         {
             RaycastHit hit;
             if (!Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
                 return new Vector3(-1,-1,-1);
 
             var tex = go.mainTexture as Texture2D;
+
+            var shockwave = Instantiate(ShockWave) as GameObject;
+            shockwave.transform.position = hit.point;
 
             var pixelUV = go.transform.InverseTransformPoint(hit.point);
             pixelUV.x += go.width / 2f;

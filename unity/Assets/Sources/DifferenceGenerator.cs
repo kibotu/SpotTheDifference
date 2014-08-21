fb232034d38ef726112786f2459b9446d27755da
Jan Rabe <jan.rabe@wooga.net>
2014-08-21 18:53:45 +0200

    floodfill to bounding rectangle

---
+++ unity/Assets/Sources/DifferenceGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Sources
+{
+    public class DifferenceGenerator : MonoBehaviour
+    {
+        public Texture2D Original;
+        public Texture2D Fake;
+
+        public UITexture Replace;
+        public float Tolerance = 2f;
+
+        public ArrayList Spots = new ArrayList();
+        public int AmountSpots;
+
+        private readonly Color _empty = new Color(0, 0, 0, 0);
+
+        public void Start()
+        {
+            Debug.Log(Original.width + " " + Original.height);
+            var tex = new Texture2D(Original.width, Original.height);
+            for (var y = 0; y < Original.height; ++y)
+            {
+                for (var x = 0; x < Original.width; ++x)
+                {
+                    var dif = Original.GetPixel(x, y) - Fake.GetPixel(x, y);
+                    if (!(dif).Equals(_empty))
+                    {
+                        // whenever there is a different color find bounding box
+//                        FloodFill.floodFill(tex, x, y);
+
+                        dif.a = 1;
+                        tex.SetPixel(x, y, Color.red);
+                    } else {
+                        tex.SetPixel(x, y, Color.black); 
+                    }
+                }
+            }
+
+            for (var y = 0; y < Original.height; ++y)
+            {
+                for (var x = 0; x < Original.width; ++x)
+                {
+                    var dif = Original.GetPixel(x, y) - Fake.GetPixel(x, y);
+                    if (!(dif).Equals(_empty))
+                    {
+                        var bounds = FloodFill.floodFill(tex, Color.blue, new Vector2(x, y));
+                        if (!bounds.Equals(new Rect()))
+                        {
+                            Spots.Add(bounds);
+                        }
+                    }
+                }
+            }
+
+            DrawFrames(tex);
+
+            Replace.mainTexture = tex;
+
+            tex.Apply();
+
+            AmountSpots = Spots.Count;
+        }
+
+        private bool IsAlreadyInFrame(Vector2 p)
+        {
+            return Spots.Cast<Rect>().Any(spot => spot.Contains(p));
+        }
+
+        private void DrawFrames(Texture2D texture2D)
+        {
+            foreach (Rect frame in Spots)
+            {
+                for (var y = (int)frame.yMin; y < frame.yMax; ++y)
+                {
+                    for (var x = (int)frame.xMin; x < frame.xMax; ++x)
+                    {
+                        texture2D.SetPixel(x, y, Color.red);
+                    }
+                }
+                Debug.Log(frame);
+            }
+        }
+    }
+}
---
+++ unity/Assets/Sources/FloodFill.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Sources
+{
+    public class FloodFill
+    {
+        /** 
+         * Fills the selected pixel and all surrounding pixels of the same color with the fill color. 
+         * @param img image on which operation is applied 
+         * @param fillColor color to be filled in 
+         * @param loc location at which to start fill 
+         * @throws IllegalArgumentException if loc is out of bounds of the image 
+         */
+        public static Rect floodFill(Texture2D img, Color fillColor, Vector2 loc)
+        {
+            if (loc.x < 0 || loc.x >= img.width || loc.y < 0 || loc.y >= img.height) throw new Exception();
+
+            var old = img.GetPixel((int) loc.x, (int) loc.y);
+
+            // Checks trivial case where loc is of the fill color  
+            if (old.Equals(fillColor))
+                return new Rect();
+
+            boundings = new Rect();
+            floodLoop(img, (int)loc.x, (int)loc.y, fillColor, old);
+            return boundings;
+        }
+
+        public static Rect boundings = new Rect();
+
+        // Recursively fills surrounding pixels of the old color  
+        private static void floodLoop(Texture2D img, int x, int y, Color fill, Color old)
+        {
+            var bounds = new Rect {width = img.width, height = img.height};
+
+            // finds the left side, filling along the way  
+            var fillL = x;
+            do
+            {
+                img.SetPixel(fillL, y, fill);
+                fillL--;
+            } while (fillL >= 0 && img.GetPixel(fillL, y).Equals(old));
+            fillL++;
+
+            // find the right right side, filling along the way  
+            var fillR = x;
+            do
+            {
+                img.SetPixel(fillR, y, fill);
+                fillR++;
+            } while (fillR < bounds.width - 1 && img.GetPixel(fillR, y).Equals(old));
+            fillR--;
+
+            // checks if applicable up or down  
+            for (var i = fillL; i <= fillR; i++)
+            {
+                if (y > 0 && img.GetPixel(i, y - 1).Equals(old)) floodLoop(img, i, y - 1, fill, old);
+                if (y < bounds.height - 1 && img.GetPixel(i, y + 1).Equals(old)) floodLoop(img, i, y + 1, fill, old);
+            }
+
+            boundings.x = Math.Min(bounds.width, fillL);
+            boundings.width = Math.Max(boundings.width, Math.Abs(fillL - fillR));
+            boundings.y = Math.Max(boundings.y, y);
+            boundings.height = 50;
+        }
+    }
+}
